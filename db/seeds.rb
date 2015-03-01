e79b6488db191a827aa94a2a8b31fc5ed352c05f
Joshua Toyota <jearsh@gmail.com>
2015-03-01 01:54:35 -0500

    cleaning up db/ from previous app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,1 @@
-require 'csv'
-
-CSV.foreach(File.join(Rails.root, 'doc/appt_data.csv'), headers: true) do |row|
-  hash = row.to_hash.with_indifferent_access
-  hash.merge!(start_time: Time.strptime(hash[:start_time], '%m/%d/%y %H:%M'),
-                end_time: Time.strptime(hash[  :end_time], '%m/%d/%y %H:%M'))
-  appt = Appointment.create(hash)
-end
 
