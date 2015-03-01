af13fbec538deacc1e6b33b6821f7e95c7ce616d
Joshua Toyota <jearsh@gmail.com>
2015-03-01 01:50:32 -0500

    testing setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+read -p 'puts application classname here (e.g. CarDetailing): ' classname
+echo
+read -p 'puts application database name here (e.g. car_detailing): ' db_name
+echo
+
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" Rakefile > tmpfile; mv -v tmpfile Rakefile
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" app/views/layouts/application.html.haml > tmpfile; mv -v tmpfile app/views/layouts/application.html.haml
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" config/application.rb > tmpfile; mv -v tmpfile config/application.rb
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" config/environment.rb > tmpfile; mv -v tmpfile config/environment.rb
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" config/environments/development.rb > tmpfile; mv -v tmpfile config/environments/development.rb
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" config/environments/production.rb > tmpfile; mv -v tmpfile config/environments/production.rb
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" config/environments/test.rb > tmpfile; mv -v tmpfile config/environments/test.rb
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" config/initializers/secret_token.rb > tmpfile; mv -v tmpfile config/initializers/secret_token.rb
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" config/initializers/session_store.rb > tmpfile; mv -v tmpfile config/initializers/session_store.rb
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" config/routes.rb > tmpfile; mv -v tmpfile config/routes.rb
+
+sed "s/GenericApplication/$classname/g;s/generic_application/$db_name/g" config/database.example.yml > tmpfile; mv -v tmpfile config/database.example.yml
+cp -v config/database{.example,}.yml
